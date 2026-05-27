18c8604511c6f2fe86f9ce42018d288e52c3a7b9
Rotjana <rotjana.pst@gmail.com>
2026-05-27 17:03:39 +0700

    first commit mine

---
+++ script.js
@@ -0,0 +1,7 @@
+// ดึงปุ่มจาก HTML มาใช้งาน
+const button = document.getElementById("myButton");
+
+// สั่งว่าถ้ามีการคลิกปุ่ม ให้ทำอะไร
+button.addEventListener("click", function() {
+    alert("ยินดีต้อนรับสู่โลกของ JavaScript! 🎉");
+});
